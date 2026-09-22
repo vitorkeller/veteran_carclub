07d14c2c633fa1821652445b774d924d0803f761
vitorkeller <vitorkeller1604@hotmail.com>
2026-09-22 16:14:30 -0300

    fix: Alteração da versão do Node, correção da Branch no CI/CD e correção do Any nos testes do frontend

--- packages/frontend/tests/AdminLoginPage.test.tsx
+++ packages/frontend/tests/AdminLoginPage.test.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
